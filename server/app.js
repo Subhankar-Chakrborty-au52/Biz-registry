6b9937c1cb5235c077be5b88e0b6ba1d9aed0d9e
unknown <aman.ap.23052001@gmail.com>
2024-02-04 11:10:23 +0530

    change branch

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(router);
-app.use(cookiParser());
+app.use(cookiParser())
 
 // Port and Server starting
 const PORT = process.env.PORT;
